c6c240861ed179951456882cb7585ea8500a3fcb
Nethmi Jayakody <83853442+neth99-coder@users.noreply.github.com>
2022-11-19 14:37:08 +0530

    window bug fix

--- client/src/views/Person/UserDashboard.jsx
+++ client/src/views/Person/UserDashboard.jsx
@@ -198,7 +198,7 @@ function UserDashboard() {
   return (
     <MDBContainer
       fluid
-      className="p-5 bg-gradient m-2"
+      className="p-5 bg-gradient m-0"
       style={{ backgroundColor: "#022D36" }}
     >
       <MDBRow>
